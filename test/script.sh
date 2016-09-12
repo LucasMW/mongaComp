617a91b6a15695372abc30bf590e94d7301e4dbf
LucasMW <great-lucas@hotmail.com>
2016-09-11 22:34:53 -0300

    Simple tests

---
+++ test/script.sh
@@ -0,0 +1,20 @@
+
+FILES="test/simple/*.monga"
+for f in $FILES
+do
+	name=$(echo $f | cut -f 1 -d '.')
+	echo "Testing $name"
+	cat $f | ./comp > $f.output
+	if [ ! -f $name.answer ]; then
+    	echo "no answer associated"
+    	continue
+	fi
+	result=$(cmp $f.output $name.answer)
+	if($result) then
+		echo "OK"
+	else
+		echo "Wrong"
+		diff $f.output $name.answer
+	fi
+done
+#cat test/file1.monga | ./comp | grep "tokens"
