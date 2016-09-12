b22d1ac670ed66ed4003fe92ffde39f681388213
LucasMW <great-lucas@hotmail.com>
2016-09-12 14:45:56 -0300

    test script now count OKs

--- test/script.sh
+++ test/script.sh
@@ -1,7 +1,11 @@
-
+#!/bin/bash
 FILES="test/simple/*.monga"
+COUNT="0"
+OK=0
+Wrongs=0
 for f in $FILES
 do
+	let COUNT=COUNT+1
 	name=$(echo $f | cut -f 1 -d '.')
 	echo "Testing $name"
 	cat $f | ./comp > $f.output
@@ -9,13 +13,14 @@ do
     	echo "no answer associated"
     	continue
 	fi
-	result=$(cmp $f.output $name.answer)
-	if($result) then
+	if(cmp $f.output $name.answer) then
 		echo "OK"
 		rm $f.output
+		let OK=OK+1
 	else
 		echo "Wrong"
 		diff $f.output $name.answer
 	fi
 done
+echo "$OK OK of $COUNT in total"
 #cat test/file1.monga | ./comp | grep "tokens"
