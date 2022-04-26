cfc97ade772784f79fff08064e2b80b1e510dcb9
LucasMW <great-lucas@hotmail.com>
2022-04-26 22:30:42 +0100

    improved implementation

--- test/examples/readFile.c
+++ test/examples/readFile.c
@@ -23,3 +23,36 @@ char * readFile(const char* path){
 	fclose(input);
 	return fileString;
 }
+int isNull(void* ptr){
+	return ptr == NULL;
+}
+
+char* readFileFast(const char* path)
+{
+	long size;
+	int read;
+	FILE * input;
+	char * fileString;
+	input = fopen(path,"rb");
+	if(!input)
+		return NULL;
+	fseek(input, 0, SEEK_END);
+	size = ftell(input);
+	rewind(input);
+	fileString = (char*)malloc(size+1);
+	if(!fileString)
+	{
+		fclose(input);
+		return NULL;
+	}
+	read = fread(fileString,1,size,input);
+	if(read != size) 
+	{
+		fclose(input);
+		free(fileString);
+		return NULL;
+	}
+	fileString[size] = '\0';
+	fclose(input);
+	return fileString;
+}
